7e3fbf094befd036e5ff87ce230b4d48da7a5e8e
StasKalina <165586905+StasKalina@users.noreply.github.com>
2024-04-16 22:27:32 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+            //this.Close();
+        }
+
+        private void Button_Click_ID_Student(object sender, RoutedEventArgs e)
+        {
+            Window1 nwc = new Window1();
+            Hide();
+            nwc.Show();
+        }
+
+        private void Krestiki_Noliki_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 nwc = new Window2();
+            Hide();
+            nwc.Show();
+
+        }
+
+        private void Culc_All_Click(object sender, RoutedEventArgs e)
+        {
+            Window3 nwc = new Window3();
+            Hide();
+            nwc.Show();
+
+        }
+
+        private void Autor_Click(object sender, RoutedEventArgs e)
+        {
+            Window4 nwc = new Window4();
+            Hide();
+            nwc.Show();
+
+        }
+    }
+}
---
+++ Window3.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        public Window3()
+        {
+            InitializeComponent();
+
+            foreach (UIElement el in MainRoot.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+
+            }
+        }
+      
+       
+        private void Button_Click(object sender, RoutedEventArgs e)
+            {
+                string str = (string)((Button)e.OriginalSource).Content;
+
+                if (str == "C")
+                    TextLabel.Text = "";
+                else if (str == "=")
+                {
+                    string value = new DataTable().Compute(TextLabel.Text, null).ToString();
+                    TextLabel.Text = value;
+                }
+                else
+                    TextLabel.Text += str;
+            }
+
+
+
+
+
+
+
+
+
+
+
+            private void GoToMainWin_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw;
+            mw = new MainWindow();
+            Hide();
+            mw.Show();
+
+        }
+
+    }
+}
